dff4a5ed041d1b3715fadc8469c0ee145a5d7992
reneepawaroo <36904984+reneepawaroo@users.noreply.github.com>
2018-04-13 22:01:00 -0400

    Add files via upload

---
+++ rockPaperScissors.js
@@ -0,0 +1,34 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "The result is a tie!";
+    }
+    else if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+    else if (userChoice === "scissors") {
+        if (computerChoice === "rock") {
+            return "rock wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+};
+
+console.log(compare(userChoice, computerChoice))
